8acf0f79affdef075004cdec6769e1e8fd0538d0
santiago <sgaviria@gmail.com>
2012-03-26 13:27:07 -0500

    initial commit

---
+++ app/app.js
@@ -0,0 +1,16 @@
+Ext.Loader.setConfig({ enabled: true });
+
+Ext.application({
+    name: 'Whiteboard',
+    views: ['Chatboard'],
+    controllers: ['MainController']
+});
+
+Ext.io.setup({
+    logLevel:'debug',
+    debug: true,
+    appId:"XB2YHJ8VyXbGt5zHhG1bqCKgTFa",
+    deviceId:"MG0gdRij06sNRCG3gC0gLnO6gGB",
+    url:"http://msg.sencha-dev.io:80",
+    transportName:'socket'
+});
---
+++ app/controller/MainController.js
@@ -0,0 +1,52 @@
+Ext.define('Whiteboard.controller.MainController', {
+    extend: 'Ext.app.Controller',
+
+    config: {
+    	refs: {
+    	    main: 'mainview',
+
+	    // Content
+	    home: 'home',
+	    workspace: 'workspace',
+
+	    // Workspace
+	    chatboard: 'chatboard',
+
+	    // Top Nav
+	    gotohome: '#gotohome',
+	    gotoworkspace: '#gotoworkspace'
+    	},
+
+	control: {
+	    // Top Nav
+	    gotohome: {
+		tap: 'navTap'
+	    },
+	    gotoworkspace: {
+		tap: 'second'
+	    }
+	}
+    },
+
+    second: function() {
+	// this.getMain().setActiveItem(1);
+    },
+
+    // Dynamically show Content views
+    // I just don't want to hardcode indexes
+    // Of course this requires to follow a convention:
+    // nav buttons must be prefixed with 'goto' followed
+    // by the named 'ref' for the content component.
+    navTap: function(c) {
+	var object= function(self) {
+	    id= c.getId().replace(/goto/,'');
+	    id= id.charAt(0).toUpperCase() + id.slice(1);
+	    return self['get'+id]();
+	}(this);
+	this.getMain().setActiveItem(object);
+    },
+
+    launch: function() {
+	Ext.create("Whiteboard.view.Chatboard");
+    }
+});
